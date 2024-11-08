f7557179981d3197516dff52d1866245958dc41a
unknown <godwinbrienth03@gmail.com>
2024-11-08 23:11:49 +0800

    third block

--- components/Package.tsx
+++ components/Package.tsx
@@ -4,7 +4,7 @@ const Package = () => {
     return <section className="flex flex-col gap-[50px]">
         <h2 className="text-[36px] font-semibold">1. Select Your Package</h2>
 
-        <div className="flex gap-[60px]">
+        <div className="flex flex-wrap gap-[60px]">
         {/* Card Essential*/}
         <div className="max-w-[370px]">
             {/* whitebackground */}
@@ -41,7 +41,7 @@ const Package = () => {
         {/* Card Standard*/}
         <div className="max-w-[370px]">
             {/* whitebackground */}
-            <div className="flex flex-col gap-[25px] border border-primary p-[16px] rounded-tl-[10px] rounded-tr-[10px]">
+            <div className="flex flex-col gap-[25px] border border-secondary p-[16px] rounded-tl-[10px] rounded-tr-[10px]">
                 <h3 className="text-[32px] font-bold text-secondary text-center">Standard</h3>
                 <div>
                 <p className="text-[20px] font-normal"><span className="font-semibold">Pages: </span>Up to 5</p>
@@ -79,7 +79,60 @@ const Package = () => {
 
             </div>
         </div>
+
+        {/* Premium Standard*/}
+        <div className="max-w-[370px]">
+            {/* whitebackground */}
+            <div className="flex flex-col gap-[25px] border border-accent p-[16px] rounded-tl-[10px] rounded-tr-[10px]">
+                <h3 className="text-[32px] font-bold text-accent text-center">Premium</h3>
+                <div>
+                <p className="text-[20px] font-normal"><span className="font-semibold">Pages: </span>Up to 10</p>
+                <p className="text-[20px] font-normal"><span className="font-semibold">Design: </span>Customized design tailored to your brand</p>
+                <p className="text-[20px] font-normal"><span className="font-semibold">Mobile: </span>Advanced mobile responsiveness</p>
+                </div>
+            </div>
+           
+            {/* yellow background */}
+            <div className="flex flex-col justify-center items-left bg-accent gap-[24px] py-[50px] px-[33px] rounded-bl-[10px] rounded-br-[10px]">
+                <div className="flex gap-[16px]">
+                    <Image src="/assets/svg/circle.svg" alt="circle" width={30} height={30  }/>
+                    <p className="text-[20px] font-semibold text-white">Contact form and CMS integration</p>
+                </div>
+                <div className="flex gap-[16px]">
+                    <Image src="/assets/svg/circle.svg" alt="circle" width={30} height={30  }/>
+                    <p className="text-[20px] font-semibold text-white">Enhanced SEO setup</p>
+                </div>
+                <div className="flex gap-[16px]">
+                    <Image src="/assets/svg/circle.svg" alt="circle" width={30} height={30  }/>
+                    <p className="text-[20px] font-semibold text-white">ChatBot integration for instant support</p>
+                </div>
+                <div className="flex gap-[16px]">
+                    <Image src="/assets/svg/circle.svg" alt="circle" width={30} height={30  }/>
+                    <p className="text-[20px] font-semibold text-white">2 Free Business Emails (Terms Apply)</p>
+                </div>
+                <div className="flex gap-[16px]">
+                    <Image src="/assets/svg/circle.svg" alt="circle" width={30} height={30  }/>
+                    <p className="text-[20px] font-semibold text-white">Analytics Integration for visitor tracking</p>
+                </div>
+                <div className="flex gap-[16px]">
+                    <Image src="/assets/svg/circle.svg" alt="circle" width={30} height={30  }/>
+                    <p className="text-[20px] font-semibold text-white">Content Management System (CMS) Integration</p>
+                </div>
+                <div className="flex gap-[16px]">
+                    <Image src="/assets/svg/circle.svg" alt="circle" width={30} height={30  }/>
+                    <p className="text-[20px] font-semibold text-white">Dedicated Project Manager</p>
+                </div>
+                <div className="flex gap-[16px]">
+                    <Image src="/assets/svg/circle.svg" alt="circle" width={30} height={30  }/>
+                    <p className="text-[20px] font-semibold text-white">Ongoing Support & Maintenance</p>
+                </div>
+
+            </div>
         </div>
+        </div>
+    
+
+        
     </section>;
     
 
