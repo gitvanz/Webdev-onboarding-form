a298a0fee9375c45efa3b1d89455f796ad5124f5
unknown <jycgraellos171@gmail.com>
2024-11-11 20:50:06 +0800

    Builder

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
-import Builder from '@/components/Builder';
-import Package from '@/components/Package';
-import WebsiteType from '@/components/WebsiteType';
+import Builder from "@/components/Builder";
+import Package from "@/components/Package";
+import WebsiteType from "@/components/WebsiteType";
 
 const Home = () => {
-    return (
-        <div className="flex flex-col justify-center items-center">
-            <h1 className="h1 mt-[50px]">
-                Website Development Onboarding Form
-            </h1>
-            <div className="flex flex-col mt-[70px] gap-[100px]">
-                <Package />
-                <Builder />
-                <WebsiteType />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center">
+      {/* <h1 className="h1 mt-[50px]">Website Development Onboarding Form</h1> */}
+      <div className="flex flex-col mt-[70px] gap-[100px]">
+        {/* <Package /> */}
+        <Builder />
+        {/* <WebsiteType /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- components/Builder.tsx
+++ components/Builder.tsx
@@ -2,149 +2,163 @@ import Image from "next/image";
 
 const WebsiteType = () => {
   return (
-    <section className="mx-[171px] flex flex-col w-[1098px] h-[1003px]">
-      <h2 className="text-[36px] font-regular w-[1098px] h-[54px]">
-        <span className="font-semibold">2. Choose Your Website Builder</span>{" "}
-        (Available with All Packages)
-      </h2>
+    <section className="flex flex-col justify-center items-end md:mx-[32px] mx-[10px]">
+      <div className="flex flex-col gap-[50px]">
+        <h2 className="sm:text-[36px] text-[24px] font-bold md:text-start text-center">
+          2. Choose Your Website Builder{" "}
+          <span className="font-normal">(Available with All Packages)</span>
+        </h2>
 
-      <div className="flex flex-col gap-[50px] w-[964px] h-[899px] mx-[64px] mt-[50px]">
-        {/* Wordpress */}
-        <div className="flex items-center justify-center gap-[50px]  w-[964px] h-[360px]">
-          <div className="flex flex-col gap-[24px] ml-[36px] w-[446px] h-[360px]">
-            {/* Wordpress heading */}
-            <h3 className="text-[32px] font-semibold">WordPress:</h3>
-            {/* Wordpress content */}
-            <div className="flex flex-col gap-[16px]">
-              <div className="flex gap-[24px]">
-                <Image
-                  src="/assets/svg/circle-check-big.svg"
-                  alt="circle-check-big.svg"
-                  width={30}
-                  height={30}
-                />
-                <div className="text-[20px] font-semibold">
-                  Best for easy content management and scalability
+        <div className="flex justify-center items-center flex-col gap-[50px]">
+          {/* Wordpress */}
+          <div className="flex flex-col lg:flex-row items-center justify-center gap-[50px]">
+            <div className="flex flex-col gap-[24px] max-w-[446px]">
+              {/* Wordpress heading */}
+              <h3 className="md:text-[32px] sm:text-[28px] text-[20px] font-semibold">
+                WordPress:
+              </h3>
+              {/* Wordpress content */}
+              <div className="flex flex-col md:gap-[16px] gap-[10px]">
+                <div className="flex gap-[16px]">
+                  <Image
+                    src="/assets/svg/circle-check-big.svg"
+                    alt="circle-check-big.svg"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="sm:text-[20px] text-[16px] font-semibold">
+                    Best for easy content management and scalability
+                  </div>
                 </div>
-              </div>
-              <div className="flex gap-[24px]">
-                <Image
-                  src="/assets/svg/circle-check-big.svg"
-                  alt="circle-check-big.svg"
-                  width={30}
-                  height={30}
-                />
-                <div className="text-[20px] font-regular">
-                  <span className="font-semibold">
+
+                <div className="sm:text-[16px] text-[10px] flex gap-[16px]">
+                  <Image
+                    src="/assets/svg/circle-check-big.svg"
+                    alt="circle-check-big.svg"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="sm:text-[20px] text-[16px] font-semibold">
                     Extensive plugin library
-                  </span>{" "}
-                  (e.g., e-commerce, booking, SEO)
+                    <span className="sm:text-[20px] text-[16px] font-normal">
+                      (e.g., e-commerce, booking, SEO)
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <div className="flex gap-[24px]">
-                <Image
-                  src="/assets/svg/circle-check-big.svg"
-                  alt="circle-check-big.svg"
-                  width={30}
-                  height={30}
-                />
-                <div className="text-[20px] font-semibold">
-                  Ideal for blogs, business websites, and e-commerce
+
+                <div className="sm:text-[16px] text-[10px] flex gap-[16px]">
+                  <Image
+                    src="/assets/svg/circle-check-big.svg"
+                    alt="circle-check-big.svg"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="sm:text-[20px] text-[16px] font-semibold">
+                    Ideal for blogs, business websites, and e-commerce
+                  </div>
                 </div>
-              </div>
-              <div className="flex gap-[24px]">
-                <Image
-                  src="/assets/svg/circle-check-big.svg"
-                  alt="circle-check-big.svg"
-                  width={30}
-                  height={30}
-                />
-                <div className="text-[20px] font-semibold w-[446px] h-[60px]">
-                  Fast setup and updates with a user-friendly backend
+
+                <div className="sm:text-[16px] text-[10px] flex gap-[16px]">
+                  <Image
+                    src="/assets/svg/circle-check-big.svg"
+                    alt="circle-check-big.svg"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="sm:text-[20px] text-[16px] font-semibold">
+                    Fast setup and updates with a user-friendly backend
+                  </div>
                 </div>
               </div>
             </div>
+
+            {/* Wordpress Image */}
+            <Image
+              src="/assets/png/WordPress.png"
+              alt="WordPress.png"
+              width={400}
+              height={250}
+              className="aspect-[8/5] sm:aspect-[4/3] w-full"
+            />
           </div>
 
-          {/* Wordpress Image */}
-          <Image
-            src="/assets/png/WordPress.png"
-            alt="WordPress.png"
-            width={400}
-            height={250}
-            className="mr-[34px] ml-[50px]"
-          />
-        </div>
+          {/* Frontend */}
+          <div className="flex flex-col lg:flex-row items-center justify-center gap-[50px]">
+            {/* Frontend Image */}
+            <Image
+              src="/assets/png/WordPress2.png"
+              alt="WordPress2.png"
+              width={400}
+              height={250}
+              className="aspect-[8/5] sm:aspect-[4/3] w-full"
+            />
 
-        {/* Frontend */}
-        <div className="flex items-center justify-center gap-[50px] w-[964px] h-[489px]">
-          {/* Frontend Image */}
-          <Image
-            src="/assets/png/WordPress2.png"
-            alt="WordPress2.png"
-            width={400}
-            height={250}
-            className="ml-[30px] "
-          />
-
-          <div className="flex flex-col gap-[24px] w-[454px] h-[489px] mr-[30px]">
-            {/* Frontend heading */}
-            <div className="flex flex-col gap-[12px]">
-              <h3 className="text-[32px] font-regular">
-                <span className="font-semibold">Front-end Framework</span>{" "}
-                (Fully Coded)
-              </h3>
-              <div className="text-[20px] font-regular">
-                (Additional Fee & Lead Time Based on Requirements){" "}
+            <div className="flex flex-col gap-[24px] max-w-[454px]">
+              {/* Frontend heading */}
+              <div className="flex flex-col gap-[12px]">
+                <h3 className="md:text-[32px] sm:text-[28px] text-[20px] font-semibold">
+                  Front-end Framework{" "}
+                  <span className="md:text-[32px] sm:text-[28px] text-[20px] font-normal">
+                    {" "}
+                    (Fully Coded)
+                  </span>
+                </h3>
+                <p className="sm:text-[20px] text-[15px]">
+                  (Additional Fee & Lead Time Based on Requirements){" "}
+                </p>
               </div>
-            </div>
-            {/* Frontend content */}
-            <div className="flex flex-col gap-[16px] mr-[30px] mt-[24px]">
-              <div className="flex gap-[24px]">
-                <Image
-                  src="/assets/svg/circle-check-big.svg"
-                  alt="circle-check-big.svg"
-                  width={30}
-                  height={30}
-                />
-                <div className="text-[20px] font-semibold">
-                  Best for easy content management and scalability
+
+              {/* Frontend content */}
+              <div className="flex flex-col gap-[16px]">
+                <div className="sm:text-[16px] text-[10px] flex gap-[16px]">
+                  <Image
+                    src="/assets/svg/circle-check-big.svg"
+                    alt="circle-check-big.svg"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="sm:text-[20px] text-[16px] font-semibold">
+                    Best for easy content management and scalability
+                  </div>
                 </div>
-              </div>
-              <div className="flex gap-[24px]">
-                <Image
-                  src="/assets/svg/circle-check-big.svg"
-                  alt="circle-check-big.svg"
-                  width={30}
-                  height={30}
-                />
-                <div className="text-[20px] font-regular">
-                  <span className="font-semibold">
+
+                <div className="sm:text-[16px] text-[10px] flex gap-[16px]">
+                  <Image
+                    src="/assets/svg/circle-check-big.svg"
+                    alt="circle-check-big.svg"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="sm:text-[20px] text-[16px] font-semibold">
                     Extensive plugin library
-                  </span>{" "}
-                  (e.g., e-commerce, booking, SEO)
+                    <span className="sm:text-[20px] text-[16px] font-normal">
+                      (e.g., e-commerce, booking, SEO)
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <div className="flex gap-[24px]">
-                <Image
-                  src="/assets/svg/circle-check-big.svg"
-                  alt="circle-check-big.svg"
-                  width={30}
-                  height={30}
-                />
-                <div className="text-[20px] font-semibold">
-                  Ideal for blogs, business websites, and e-commerce
+
+                <div className="sm:text-[16px] text-[10px] flex gap-[16px]">
+                  <Image
+                    src="/assets/svg/circle-check-big.svg"
+                    alt="circle-check-big.svg"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="sm:text-[20px] text-[16px] font-semibold">
+                    Ideal for blogs, business websites, and e-commerce
+                  </div>
                 </div>
-              </div>
-              <div className="flex gap-[24px]">
-                <Image
-                  src="/assets/svg/circle-check-big.svg"
-                  alt="circle-check-big.svg"
-                  width={30}
-                  height={30}
-                />
-                <div className="text-[20px] font-semibold">
-                  Fast setup and updates with a user-friendly backend
+
+                <div className="sm:text-[16px] text-[10px] flex gap-[16px]">
+                  <Image
+                    src="/assets/svg/circle-check-big.svg"
+                    alt="circle-check-big.svg"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="sm:text-[20px] text-[16px] font-semibold">
+                    Fast setup and updates with a user-friendly backend
+                  </div>
                 </div>
               </div>
             </div>
